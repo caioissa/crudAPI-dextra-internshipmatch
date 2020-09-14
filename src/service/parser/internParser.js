092525e14bf2c53110ac9c69a181f38bcf2866ca
caioissa <caioissa96@gmail.com>
2020-09-14 12:02:10 -0300

    Added age and bio to intern

--- src/service/parser/internParser.js
+++ src/service/parser/internParser.js
@@ -1,5 +1,6 @@
 const { toArray } = require('../../utils/array');
 const { getUserFromEmail } = require('../../utils/username');
+const { getAgeFromBirthdate } = require('../../utils/birthdate');
 
 const parser = (row, i) => ({
     id: i,
@@ -10,7 +11,9 @@ const parser = (row, i) => ({
     knows: toArray(row['Tecnologias que tem experiência']),
     wants: toArray(row['Tecnologias que quer aprender']),
     choices: toArray(row['Escolhas']),
-    languages: toArray(row['Idiomas'])
+    languages: toArray(row['Idiomas']),
+    bio: row['Descreva você'],
+    age: getAgeFromBirthdate(row['Data de Nascimento'])
 })
 
 module.exports = { parser };
---
+++ src/utils/birthdate.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+
+const getAgeFromBirthdate = birthdate => {
+    const bd = moment(birthdate, "MM/DD/YYYY");
+    const today = moment();
+    return today.diff(bd, 'years');
+}
+
+module.exports = { getAgeFromBirthdate };
